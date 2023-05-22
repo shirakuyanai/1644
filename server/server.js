513d9f28fa739ff077e8d299b22e62be3a1bffd0
kydzuwu <kydz.uwu@gmail.com>
2023-05-22 17:00:40 +0700

    Adding CRUD for product

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = 5000
-const cors = require('cors')
 const mongoose = require('mongoose')
 app.use(express.json())
 app.use(cors({
@@ -36,11 +35,13 @@ app.post('/newbrand', (req, res) => {
     res.json(brand)
 })
 
+
+// products
 app.get('/products', async(req, res) => {
     const products = await Product.find()
     res.json(products)
 })
-
+//add product
 app.post('/newproduct', async(req, res) => {
     const product = await new Product(
         {
@@ -55,5 +56,50 @@ app.post('/newproduct', async(req, res) => {
     product.save()
     res.json(product)
 })
+//edit product
+app.put('/products/edit/:id', async (req, res) => {
+    const id = req.params.id;
+    const prod = await Product.findById(id);
+    
+    if (!prod) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+    
+    prod.name = req.body.name;
+    prod.brand = req.body.brand;
+    prod.price = req.body.price;
+    prod.description = req.body.description;
+    prod.colors = req.body.colors;
+    prod.specs = req.body.specs;
+    
+    await prod.save();
+    
+    res.json(prod);
+  });
+  //delete product
+  app.delete('/products/delete/:id', async(req, res) => {
+    const id = req.params.id;
+    const prod = await Product.findByIdAndDelete(id);
+   
+    res.json(prod)
+})
+
+
+  
+
+//Order
+
+app.get('/orders', async(req, res) => {
+    const order = await Order.find()
+    res.json(order)
+})
+
+app.post('/neworder', async(req, res) => {
+    
+})
+
+
+
+
 
 app.listen(port, () => console.log(`listening on port ${port}`))
