852a90d8d0215771989ab86cda5a6f9d58d35a80
Shiraku Yanai <shirakuyanai@gmail.com>
2023-06-27 10:47:59 +0700

    feat cookie policies

--- server/server.js
+++ server/server.js
@@ -41,7 +41,12 @@ app.use(session({
   secret: process.env.SESSION_SECRET_KEY,
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: null }
+  cookie: { 
+    httpOnly: true, 
+    secure: true, 
+    maxAge: 1000 * 60 * 60 * 48, 
+    sameSite: 'none' 
+  }
 }))
 
 app.use(cors({
